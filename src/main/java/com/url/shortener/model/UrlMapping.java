1cfa06f4598c15445f863b0699afece605000d3e
katshashank45 <katshashank45@gmail.com>
2025-02-17 08:55:26 +0530

    Lombok was not working in Intellij Idea so generated getters and setters

--- src/main/java/com/url/shortener/model/UrlMapping.java
+++ src/main/java/com/url/shortener/model/UrlMapping.java
@@ -16,6 +16,62 @@ public class UrlMapping {
 
     private String originalUrl;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public int getClickCount() {
+        return clickCount;
+    }
+
+    public void setClickCount(int clickCount) {
+        this.clickCount = clickCount;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<ClickEvent> getClickEvents() {
+        return clickEvents;
+    }
+
+    public void setClickEvents(List<ClickEvent> clickEvents) {
+        this.clickEvents = clickEvents;
+    }
+
     private String shortUrl;
 
     private int clickCount = 0;
